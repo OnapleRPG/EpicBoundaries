458bce4f61bcd04fed1c43ec0cc5d8953ae9725b
zessirb <zessirb@gmail.com>
2018-07-15 23:31:16 +0200

    Added README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# EpicBoundaries   [![Build Status](https://travis-ci.org/OnapleRPG/EpicBoundaries.svg?branch=master)](https://travis-ci.org/OnapleRPG/EpicBoundaries) ![Sonarcloud Status](https://sonarcloud.io/api/project_badges/measure?project=EpicBoundaries&metric=alert_status)  [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+## Introduction  
+EpicBoundaries is a Sponge Minecraft plugin that enables the use of **world instances**. 
+Use the included commands to travel through the instances that have been configured in the configuration file, or include 
+this plugin as a dependency in your own plugin to use the instance features.
--- src/main/java/com/onaple/epicboundaries/EpicBoundaries.java
+++ src/main/java/com/onaple/epicboundaries/EpicBoundaries.java
@@ -1,34 +1,9 @@
 package com.onaple.epicboundaries;
 
 
-import ninja.leaping.configurate.objectmapping.ObjectMappingException;
-import org.slf4j.Logger;
-import org.spongepowered.api.Sponge;
-import org.spongepowered.api.asset.Asset;
-import org.spongepowered.api.command.args.GenericArguments;
-import org.spongepowered.api.command.spec.CommandSpec;
-import org.spongepowered.api.config.ConfigDir;
-import org.spongepowered.api.data.key.Keys;
-import org.spongepowered.api.entity.Entity;
-import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.cause.entity.damage.source.EntityDamageSource;
-import org.spongepowered.api.event.entity.DestructEntityEvent;
-import org.spongepowered.api.event.entity.InteractEntityEvent;
-import org.spongepowered.api.event.filter.cause.Root;
 import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.plugin.Plugin;
-import org.spongepowered.api.plugin.PluginContainer;
-import org.spongepowered.api.text.BookView;
-import org.spongepowered.api.text.Text;
-import org.spongepowered.api.world.World;
-
-import javax.inject.Inject;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 
 @Plugin(id = "epicboundaries", name = "EpicBoundaries", version = "0.1.1")
