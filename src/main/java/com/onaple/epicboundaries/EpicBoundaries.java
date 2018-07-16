47f618c4a0a3c6bfb36606c8942944f7dd1cff6b
Zessirb <zessirb@gmail.com>
2018-07-16 14:55:55 +0200

    Added apparate command

--- src/main/java/com/onaple/epicboundaries/EpicBoundaries.java
+++ src/main/java/com/onaple/epicboundaries/EpicBoundaries.java
@@ -1,20 +1,42 @@
 package com.onaple.epicboundaries;
 
 
+import com.onaple.epicboundaries.commands.ApparateCommand;
+import org.slf4j.Logger;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.args.GenericArguments;
+import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.text.Text;
+
+import javax.inject.Inject;
 
 
 @Plugin(id = "epicboundaries", name = "EpicBoundaries", version = "0.1.1")
 public class EpicBoundaries {
-    private static EpicBoundaries instance;
-    public static EpicBoundaries getInstance() {
-        return instance;
+    private static Logger logger;
+    @Inject
+    private void setLogger(Logger logger) {
+        EpicBoundaries.logger = logger;
+    }
+    public static Logger getLogger() {
+        return logger;
     }
 
     @Listener
     public void onServerStart(GameInitializationEvent event) {
-        instance = this;
+        CommandSpec apparateSpec = CommandSpec.builder()
+                .description(Text.of("Apparate player to another world"))
+                .permission("epicboundaries.command.apparate")
+                .arguments(GenericArguments.onlyOne(GenericArguments.string(Text.of("world"))),
+                        GenericArguments.optional(GenericArguments.player(Text.of("player"))))
+                .executor(new ApparateCommand())
+                .build();
+
+        Sponge.getCommandManager().register(this, apparateSpec, "apparate");
+
+        logger.info("EPICBOUNDARIES initialized.");
     }
 }
---
+++ src/main/java/com/onaple/epicboundaries/commands/ApparateCommand.java
@@ -0,0 +1,49 @@
+package com.onaple.epicboundaries.commands;
+
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.world.World;
+
+import java.util.Optional;
+
+public class ApparateCommand implements CommandExecutor {
+    /**
+     * Apparate a player in a given world
+     * @param src Origin of the command
+     * @param args Arguments of the command
+     * @return Command result state
+     * @throws CommandException Command error
+     */
+    @Override
+    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+        String worldName = args.<String>getOne("world").orElse("world");
+
+        Optional<Player> playerOpt = args.getOne("player");
+        Player player = null;
+        if (!playerOpt.isPresent()) {
+            if (src instanceof Player) {
+                player = (Player) src;
+            } else {
+                src.sendMessage(Text.of("There must be a player target."));
+                return CommandResult.empty();
+            }
+        } else {
+            player = playerOpt.get();
+        }
+
+        Optional<World> worldOpt = Sponge.getServer().getWorld(worldName);
+        if (!worldOpt.isPresent()) {
+            src.sendMessage(Text.of("World " + worldName + " doesn't exist."));
+            return CommandResult.empty();
+        }
+
+        player.transferToWorld(worldOpt.get());
+        return CommandResult.success();
+    }
+}
